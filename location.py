872b05f35da169fa267d83bf3f2caa2f971f2b03
darshavj <darshavj094@gmail.com>
2023-10-09 12:53:58 +0530

    Track phone number using python and track mobile number locations

---
+++ location.py
@@ -0,0 +1,10 @@
+import phonenumbers
+from num import number
+
+from phonenumbers import geocoder
+
+pepnumber = phonenumbers.parse(number)
+
+location = geocoder.description_for_number(pepnumber,"en")
+
+print(location)
